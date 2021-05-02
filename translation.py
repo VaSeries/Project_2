6e2f69eff7ad73c62687f28a04d4f105a9f17810
VaSeries <82926176+VaSeries@users.noreply.github.com>
2021-05-02 05:50:41 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,7 +6,7 @@ This is a Telegram URL Upload Bot!
 
 /help for more details..
 
-Creator: MHDMILHEM
+Creator: @MHDMILHEM
 
 Support Channel : @VaSeries, @adobefiles"""
     
@@ -17,8 +17,8 @@ Support Channel : @VaSeries, @adobefiles"""
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = "Trying to download"
+    UPLOAD_START = "Trying to upload"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @VaSeries , @adobefiles"
